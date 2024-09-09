d54cd0f0129c404464208f24ec1379ea6e7f1cc3
Musk <inhoyoo1029@gmail.com>
2024-09-09 18:05:43 +0900

    Initial Commit

---
+++ RotationTest/RotationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationTestApp.swift
+//  RotationTest
+//
+//  Created by Musk on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RotationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
